8f22f9efe3d42cf2d8112231a3c9f89385a993ce
Ivan <ivan.bazhenov@gmail.com>
2019-01-24 19:07:45 +0700

    change design app layout and navbar

--- resources/js/components/Navbar.vue
+++ resources/js/components/Navbar.vue
@@ -1,105 +1,83 @@
 <template>
-  <Header :style="{position: 'fixed', width: '100%', zIndex: '9999999999'}">
-    <Menu mode="horizontal" theme="dark" active-name="1">
-      <div class="layout-logo"></div>
-        <div class="layout-nav">
-          <MenuItem name="1">
-            <router-link :to="{name: 'home'}" class="navbar-brand">
-              {{ appName }}
-            </router-link>
-          </MenuItem>
-          <MenuItem name="2">
-            <Icon type="ios-keypad"></Icon>
-            {{ $t('category_expenses') }}
-          </MenuItem>
-
-            <Row type="flex" justify="end" align="middle" class="user-dropdown-innercon">
-                <MenuItem name="3">
-                    <locale-dropdown/>
-                </MenuItem>
-            </Row>
-
-          <div v-if="user" class="user-dropdown-menu-con">
-            <Row type="flex" justify="end" align="middle" class="user-dropdown-innercon">
-              <MenuItem name="3">
-                  <locale-dropdown/>
-              </MenuItem>
-              <Dropdown transfer trigger="click" >
-                  <a href="javascript:void(0)">
-                    <span class="main-user-name">{{ user.name }}</span>
-                    <Icon type="arrow-down-b"></Icon>
-                  </a>
-                  <DropdownMenu slot="list">
-                    <router-link :to="{ name: 'settings.profile' }" >
-                      <DropdownItem name="ownSpace">
-                        <Icon type="ios-gear-outline"></Icon>
-                        {{ $t('settings') }}
-                      </DropdownItem>
-                    </router-link>
-                    <a @click.prevent="logout" href="#">
-                      <DropdownItem name="logout">
-                        <Icon type="log-out"></Icon>
-                        {{ $t('logout') }}
-                      </DropdownItem>
-                    </a>
-                  </DropdownMenu>
-              </Dropdown>
-              <Avatar :src="user.photo_url" style="background: #619fe7;margin-left: 10px;"></Avatar>
-            </Row>
-          </div>
-
-          <!-- Guest -->
-          <!--<template v-else>-->
-             <!--<Row type="flex" justify="end" align="middle" class="user-dropdown-innercon">-->
-            <!--<MenuItem name="5">-->
-              <!--<router-link :to="{ name: 'login' }" class="nav-link" active-class="active">-->
-                <!--<Button>-->
-                  <!--<Icon type="log-in"></Icon>-->
-                  <!--{{ $t('login') }}-->
-                <!--</Button>-->
-              <!--</router-link>-->
-            <!--</MenuItem>-->
-            <!--<MenuItem name="6">-->
-              <!--<router-link :to="{ name: 'register' }" class="nav-link" active-class="active">-->
-                <!--<Button>-->
-                  <!--<Icon type="ios-personadd"></Icon>-->
-                  <!--{{ $t('register') }}-->
-                <!--</Button>-->
-              <!--</router-link>-->
-            <!--</MenuItem>-->
-          <!--</Row>-->
-        <!--</template>-->
-      </div>
-    </Menu>
-  </Header>
+    <Header class="header">
+        <Menu mode="horizontal" theme="light" active-name="1">
+            <div class="layout-logo">
+                <router-link :to="{ name: 'home' }">
+                    {{ appName }}
+                </router-link>
+            </div>
+            <div class="layout-nav">
+                <Row type="flex" justify="end" align="middle">
+                    <Menu mode="horizontal" on-select theme="light" active-name="1">
+                        <locale-dropdown/>
+                    </Menu>
+                </Row>
+            </div>
+        </Menu>
+    </Header>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import LocaleDropdown from './LocaleDropdown'
+    import {mapGetters} from 'vuex'
+    import LocaleDropdown from './LocaleDropdown'
 
-export default {
-  data: () => ({
-    appName: window.config.appName
-  }),
+    export default {
+        data: () => ({
+            appName: window.config.appName
+        }),
 
-  computed: mapGetters({
-    user: 'auth/user'
-  }),
+        computed: mapGetters({
+            user: 'auth/user'
+        }),
 
-  components: {
-    LocaleDropdown
-  },
+        components: {
+            LocaleDropdown
+        },
 
-  methods: {
-    async logout () {
-      // Log out the user.
-      await this.$store.dispatch('auth/logout')
+        methods: {
+            async logout() {
+                // Log out the user.
+                await this.$store.dispatch('auth/logout');
 
-      // Redirect to login.
-      this.$router.push({ name: 'login' })
+                // Redirect to login.
+                this.$router.push({name: 'login'})
+            }
+        }
     }
-  }
-}
 </script>
+
+<style scoped>
+
+    .layout-nav {
+        width: 100%;
+        margin: 0 20px 0 auto;
+    }
+
+    .layout-logo {
+        float: left;
+        font-weight: bold;
+        font-variant: petite-caps;
+    }
+
+    .layout-logo a {
+        color: #2d8cf0;
+        font-size: 20px;
+    }
+
+    .layout-logo a:hover {
+        color: #2b85e4;
+    }
+
+    .header {
+        background: #fff;
+        height: 60px;
+        border-bottom: 1px solid #dcdee2;
+    }
+
+    .logo {
+        height: 35px;
+        position: relative;
+        top: 9px;
+    }
+</style>
 
--- resources/js/layouts/app.vue
+++ resources/js/layouts/app.vue
@@ -1,50 +1,125 @@
 <template>
-  <div class="layout">
-    <Layout style="min-height:100vh">
-      <navbar></navbar>
-        <Content  :style="{margin: '88px 60px 0'}" >
-          <child/>
-        </Content>
-      <Footer class="layout-footer-center">2017-2018 &copy; DeltaCodes</Footer>
-    </Layout>
-  </div>
+    <div class="layout">
+        <Layout style="min-height:100vh">
+            <navbar></navbar>
+            <Layout>
+                <Sider :style="{background: '#fff', minWidth: '220px'}" breakpoint="md" collapsible
+                       :collapsed-width="78"
+                       class="border-slider">
+                    <Menu active-name="1" theme="primary" width="auto">
+                        <MenuGroup :title="$t('navigation')" style="padding-top: 10px;">
+                            <MenuItem name="home" :to="{ name: 'home'}">
+                                <Icon type="ios-analytics-outline"/>
+                                {{ $t('home') }}
+                            </MenuItem>
+                        </MenuGroup>
+                    </Menu>
+                    <div slot="trigger"></div>
+                    <div class="menu-footer">
+                        <p>2019 &copy; Ivan Bazhenov</p>
+                        <p>
+
+                        </p>
+                    </div>
+                </Sider>
+                <Content style="padding: 0 24px 24px; min-height: 280px; background: #f5f7f9;">
+                    <div style="margin: 24px 0"></div>
+                    <template>
+                        <child/>
+                    </template>
+                </Content>
+            </Layout>
+        </Layout>
+    </div>
 </template>
 
 <script>
-import Navbar from '~/components/Navbar'
+    import Navbar from '~/components/Navbar'
 
-export default {
-  name: 'app-layout',
-
-  components: {
-    Navbar
-  }
-}
+    export default {
+        name: 'app-layout',
+        data() {
+            return {
+                isCollapsed: false,
+                showCard: false
+            };
+        },
+        components: {
+            Navbar
+        },
+        watch: {
+            '$route'(to, from) {
+                // this.navigateTo(to.name);
+                this.showCard = to.name !== 'home';
+            }
+        }
+    }
 </script>
 <style scoped>
-    .layout{
-        border: 1px solid #d7dde4;
+    .layout {
         background: #f5f7f9;
         position: relative;
-        border-radius: 4px;
         overflow: hidden;
     }
-    .layout-logo{
-        width: 100px;
-        height: 30px;
-        background: #5b6270;
-        border-radius: 3px;
-        float: left;
-        position: relative;
-        top: 15px;
-        left: 20px;
+
+    .border-slider::after {
+        content: '';
+        display: block;
+        width: 1px;
+        height: 100%;
+        background: #dcdee2;
+        position: absolute;
+        top: 0;
+        bottom: 0;
+        right: 0;
+        z-index: 1;
     }
-    .layout-nav{
-        width: 420px;
-        margin: 0 auto;
-        margin-right: 20px;
+
+    .menu-item span {
+        display: inline-block;
+        overflow: hidden;
+        width: 69px;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        vertical-align: bottom;
+        transition: width .2s ease .2s;
     }
-    .layout-footer-center{
+
+    .menu-item i {
+        transform: translateX(0px);
+        transition: font-size .2s ease, transform .2s ease;
+        vertical-align: middle;
+        font-size: 18px;
+    }
+
+    .collapsed-menu span {
+        width: 0;
+        transition: width .2s ease;
+    }
+
+    .collapsed-menu i {
+        transform: translateX(5px);
+        transition: font-size .2s ease .2s, transform .2s ease .2s;
+        vertical-align: middle;
+        font-size: 20px;
+    }
+
+    .menu-footer {
+        position: fixed;
+        bottom: 15px;
         text-align: center;
+    }
+
+    .menu-footer p {
+        width: 220px;
+        line-height: 25px;
+    }
+
+    .menu-footer a {
+        color: #9ea7b4;
+    }
+
+    .menu-footer a:hover {
+        color: #657180;
     }
 </style>
